2b7f0f9233950fbec8f7001a78e83714f1a2bd4e
Nathan L <adamnate@gmail.com>
2016-05-01 20:26:22 -0700

    update works

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -23,7 +23,7 @@ class CreaturesController < ApplicationController
 
       # if creature saves, redirect to route that displays all creatures
       if creature.save
-        redirect_to creatures_path
+        redirect_to creatures_path(creature)
         # redirect_to creatures_path is equivalent to:
         # redirect_to "/creatures"
       end
@@ -41,5 +41,39 @@ class CreaturesController < ApplicationController
       # render the show view (it has access to instance variable)
       render :show
       end
+
+      # show the edit creature form
+     def edit
+       # get the creature id from the url params
+       creature_id = params[:id]
+
+       # use `creature_id` to find the creature in the database
+       # and save it to an instance variable
+       @creature = Creature.find_by_id(creature_id)
+
+       # render the edit view (it has access to instance variable)
+       render :edit
+     end
+
+     # update a creature in the database
+    def update
+      # get the creature id from the url params
+      creature_id = params[:id]
+
+      # use `creature_id` to find the creature in the database
+      # and save it to an instance variable
+      creature = Creature.find_by_id(creature_id)
+
+      # whitelist params and save them to a variable
+      creature_params = params.require(:creature).permit(:name, :description)
+
+      # update the creature
+      creature.update_attributes(creature_params)
+
+      # redirect to show page for the updated creature
+      redirect_to creature_path(creature)
+      # redirect_to creature_path(creature) is equivalent to:
+      # redirect_to "/creatures/#{creature.id}"
+    end
 
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   get "/creatures/new", to: "creatures#new"
   post "/creatures", to: "creatures#create"
   get "/creatures/:id", to: "creatures#show", as: "creature"
+  get "/creatures/:id/edit", to: "creatures#edit", as: "edit_creature"
+  patch "/creatures/:id", to: "creatures#update"
 end
